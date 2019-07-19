74920842551c95af0005a5d7848ecfdc2aa946c2
Jesse Ma <j.masonoma17@gmail.com>
2019-07-19 15:28:10 -0700

    Initial CRA - setup

---
+++ luxury-test/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import MainBody from '../MainBody/MainBody';
+
+const App = () => {
+  return (
+    <div className="App">
+      <MainBody />
+    </div>
+  );
+};
+
+export default App;
